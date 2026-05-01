a5ac769f2db85c039f32add21f2e8161a866728a
Juan Carlos Beaskoetxea <juan@bidedigitala.eus>
2026-05-01 19:58:35 +0200

    'plantillas'

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,46 @@
 
 	namespace App\Controller;
 
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Routing\Attribute\Route;
 	
-	class MainController
+	class MainController extends AbstractController
 	{
-        #[Route('/')]
+        #[Route('/', name: 'home')]
         public function homepage(): Response
         {
-            return new Response('<strong>Starshop</strong>: tu tienda especializada en repuestos para naves espaciales');
+            $totalTareas = 457;   // Valor ficticio por ahora
+		
+		    $tareaEjemplo = [
+                'titulo' => 'Revisar informe trimestral',
+                'prioridad' => 'alta',
+                'estado' => 'pendiente',
+                'fechaLimite' => '2026-05-15',
+            ];
+                
+            return $this->render('main/homepage.html.twig', [
+                'totalTareas' => $totalTareas,
+                'tarea' => $tareaEjemplo,
+            ]);
         }
+
+        #[Route('/sobre-mi', name: 'about')]
+		public function about(): Response
+		{
+			return $this->render('main/about.html.twig');
+		}
+		
+		#[Route('/servicios', name: 'services')]
+		public function services(): Response
+		{
+			return $this->render('main/services.html.twig');
+		}
+		
+		#[Route('/contacto', name: 'contact')]
+		public function contact(): Response
+		{
+			return $this->render('main/contact.html.twig');
+		}
+
 	}
